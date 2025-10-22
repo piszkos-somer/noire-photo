394c2312395e855deb70f9b6c3ee065dec1e72f9
piszkos-somer <peratt816@hengersor.hu>
2025-10-22 13:11:00 +0200

    port beírva MEGYEN
    csak a portot kellett volna átállítani

--- backend/server.js
+++ backend/server.js
@@ -33,7 +33,7 @@ const pool = mysql.createPool({
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "",
   database: process.env.DB_NAME || "noire",
-  port: process.env.DB_PORT || 3306,
+  port: process.env.DB_PORT || 3307,
 });
 
 app.post("/upload", upload.single("image"), async (req, res) => {
