fb8e9cb85db4cf2c255e45c54c1113785d504a0b
piszkos-somer <peratt816@hengersor.hu>
2025-10-27 18:38:12 +0200

    bio/profilkép rekordok
    adatbázis előkészítve bio és profilkép beírásához

--- noire.sql
+++ noire.sql
@@ -1,23 +1,32 @@
--- phpMyAdmin SQL Dump (üres adatbázis)
--- Verzió: 5.2.1
--- Kiszolgáló: 127.0.0.1:3307
--- Létrehozva: 2025-10-22
--- MariaDB verzió: 10.4.28
--- PHP verzió: 8.2.4
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Gép: 127.0.0.1
+-- Létrehozás ideje: 2025. Okt 27. 17:37
+-- Kiszolgáló verziója: 10.4.32-MariaDB
+-- PHP verzió: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-CREATE DATABASE IF NOT EXISTS `noire`
-  DEFAULT CHARACTER SET utf8mb4
-  COLLATE utf8mb4_general_ci;
 
-USE `noire`;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Adatbázis: `noire`
+--
 
 -- --------------------------------------------------------
--- Tábla: `comments`
--- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `comments`
+--
+
 CREATE TABLE `comments` (
   `id` int(10) UNSIGNED NOT NULL,
   `user_id` int(10) UNSIGNED NOT NULL,
@@ -27,8 +36,11 @@ CREATE TABLE `comments` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
--- Tábla: `images`
--- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `images`
+--
+
 CREATE TABLE `images` (
   `id` int(10) UNSIGNED NOT NULL,
   `user_id` int(10) UNSIGNED NOT NULL,
@@ -38,9 +50,20 @@ CREATE TABLE `images` (
   `url` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- A tábla adatainak kiíratása `images`
+--
+
+INSERT INTO `images` (`id`, `user_id`, `title`, `description`, `upload_date`, `url`) VALUES
+(1, 1, 'tigriske', 'ez egy szép tigris petzd', '2025-10-27 17:17:15', '/images/1761578235052.jpg'),
+(2, 1, 'Mercedes', 'A legszebb merci amit valaha láttam!', '2025-10-27 17:43:22', '/images/1761579802620.jpg');
+
 -- --------------------------------------------------------
--- Tábla: `image_reactions`
--- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `image_reactions`
+--
+
 CREATE TABLE `image_reactions` (
   `id` int(10) UNSIGNED NOT NULL,
   `image_id` int(10) UNSIGNED NOT NULL,
@@ -49,116 +72,208 @@ CREATE TABLE `image_reactions` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
--- Tábla: `image_tags`
--- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `image_tags`
+--
+
 CREATE TABLE `image_tags` (
   `image_id` int(10) UNSIGNED NOT NULL,
   `tag_id` int(10) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- A tábla adatainak kiíratása `image_tags`
+--
+
+INSERT INTO `image_tags` (`image_id`, `tag_id`) VALUES
+(1, 2),
+(2, 4),
+(2, 5);
+
 -- --------------------------------------------------------
--- Tábla: `reactions`
--- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `reactions`
+--
+
 CREATE TABLE `reactions` (
   `id` int(10) UNSIGNED NOT NULL,
   `reaction_type` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
--- Tábla: `tags`
--- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `tags`
+--
+
 CREATE TABLE `tags` (
   `id` int(10) UNSIGNED NOT NULL,
   `tag` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- A tábla adatainak kiíratása `tags`
+--
+
+INSERT INTO `tags` (`id`, `tag`) VALUES
+(5, 'Car'),
+(4, 'Mercedes'),
+(2, 'tigris');
+
 -- --------------------------------------------------------
--- Tábla: `users`
--- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `users`
+--
+
 CREATE TABLE `users` (
   `id` int(10) UNSIGNED NOT NULL,
   `username` varchar(50) NOT NULL,
   `email` varchar(100) NOT NULL,
-  `password` varchar(255) NOT NULL
+  `password` varchar(255) NOT NULL,
+  `bio` varchar(500) DEFAULT NULL,
+  `profile_picture` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
--- Indexek
--- --------------------------------------------------------
+--
+-- A tábla adatainak kiíratása `users`
+--
 
+INSERT INTO `users` (`id`, `username`, `email`, `password`, `bio`, `profile_picture`) VALUES
+(1, 'somer fatera', 'perakattis@gmail.com', '$2b$10$vBS.xhlLasGMI3DPjjr6r.8MlEsnH5wFVAr/V64MXllFI4gaMS76O', NULL, NULL);
+
+--
+-- Indexek a kiírt táblákhoz
+--
+
+--
+-- A tábla indexei `comments`
+--
 ALTER TABLE `comments`
   ADD PRIMARY KEY (`id`),
   ADD KEY `user_id` (`user_id`),
   ADD KEY `image_id` (`image_id`);
 
+--
+-- A tábla indexei `images`
+--
 ALTER TABLE `images`
   ADD PRIMARY KEY (`id`),
   ADD KEY `user_id` (`user_id`);
 
+--
+-- A tábla indexei `image_reactions`
+--
 ALTER TABLE `image_reactions`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `unique_user_image` (`user_id`,`image_id`),
   ADD KEY `image_id` (`image_id`),
   ADD KEY `reaction_id` (`reaction_id`);
 
+--
+-- A tábla indexei `image_tags`
+--
 ALTER TABLE `image_tags`
   ADD PRIMARY KEY (`image_id`,`tag_id`),
   ADD KEY `tag_id` (`tag_id`);
 
+--
+-- A tábla indexei `reactions`
+--
 ALTER TABLE `reactions`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `reaction_type` (`reaction_type`);
 
+--
+-- A tábla indexei `tags`
+--
 ALTER TABLE `tags`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `tag` (`tag`);
 
+--
+-- A tábla indexei `users`
+--
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `username` (`username`),
   ADD UNIQUE KEY `email` (`email`);
 
--- --------------------------------------------------------
--- AUTO_INCREMENT értékek
--- --------------------------------------------------------
+--
+-- A kiírt táblák AUTO_INCREMENT értéke
+--
 
+--
+-- AUTO_INCREMENT a táblához `comments`
+--
 ALTER TABLE `comments`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
+--
+-- AUTO_INCREMENT a táblához `images`
+--
 ALTER TABLE `images`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
+--
+-- AUTO_INCREMENT a táblához `image_reactions`
+--
 ALTER TABLE `image_reactions`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
+--
+-- AUTO_INCREMENT a táblához `reactions`
+--
 ALTER TABLE `reactions`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
+--
+-- AUTO_INCREMENT a táblához `tags`
+--
 ALTER TABLE `tags`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
+--
+-- AUTO_INCREMENT a táblához `users`
+--
 ALTER TABLE `users`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
--- --------------------------------------------------------
--- Idegen kulcsok
--- --------------------------------------------------------
+--
+-- Megkötések a kiírt táblákhoz
+--
 
+--
+-- Megkötések a táblához `comments`
+--
 ALTER TABLE `comments`
   ADD CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
   ADD CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`image_id`) REFERENCES `images` (`id`) ON DELETE CASCADE;
 
+--
+-- Megkötések a táblához `images`
+--
 ALTER TABLE `images`
   ADD CONSTRAINT `images_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
 
+--
+-- Megkötések a táblához `image_reactions`
+--
 ALTER TABLE `image_reactions`
   ADD CONSTRAINT `image_reactions_ibfk_1` FOREIGN KEY (`image_id`) REFERENCES `images` (`id`) ON DELETE CASCADE,
   ADD CONSTRAINT `image_reactions_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
   ADD CONSTRAINT `image_reactions_ibfk_3` FOREIGN KEY (`reaction_id`) REFERENCES `reactions` (`id`) ON DELETE CASCADE;
 
+--
+-- Megkötések a táblához `image_tags`
+--
 ALTER TABLE `image_tags`
   ADD CONSTRAINT `image_tags_ibfk_1` FOREIGN KEY (`image_id`) REFERENCES `images` (`id`) ON DELETE CASCADE,
   ADD CONSTRAINT `image_tags_ibfk_2` FOREIGN KEY (`tag_id`) REFERENCES `tags` (`id`) ON DELETE CASCADE;
-
 COMMIT;
 
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
