843bb22b4d2c044e7ec401e9217442f923eaa571
piszkos-somer <peratt816@hengersor.hu>
2025-11-04 13:13:52 +0100

    server_test deleted

--- backend/server_test.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const app = express();
-
-const PORT = 3001;
-app.get("/", (req, res) => res.send("Szerver él!"));
-app.listen(PORT, () => console.log(`✅ Teszt szerver fut a ${PORT} porton!`));
