31079eb230f8ffc55ec4b61e5e79a813c5d8c37a
Kzsombor5 <kabai.zsombi@gmail.com>
2025-11-06 13:35:58 +0100

    aboutoldal

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Container } from 'react-bootstrap';
 import "../css/About.css";
 
+
 function About() {
   return (
     <Container className="py-5">
@@ -16,16 +17,19 @@ function About() {
 
       <div className="about-grid">
         {/* Attila */}
-        <div className="about-item text-box">Sziasztok Attila vagyok.</div>
-        <div className="about-item image-box">Kép Attiláról</div>
+        <div className="about-item text-box">Perák Attila vagyok, és számomra öröm, hogy ilyen kedves és okos kortársaimmal dolgozhattam együtt ezen a mestermunkán. Szeretem az informatikát, és büszke tanulója vagyok a Pestszentlőrinci Technikumnak. A mestermunka elkészítése soán jobban megismerhettem a NodeJS fejlesztői környezetét és sokat tanultam a controller elkészítésében. Szívesen segítettem másoknak, ha problémába ütköztek, és kifogástalanul együttműködtünk. Bízok abban, hogy a weboldalunk képes egy olyan fotós közösség megteremtésére, amelyet a jóindulat és a fejlődés vágya vezérel, és ahol a felhasználók elégedetten szereznek, vagy írnak építő jellegű, hiteles kritikát és javaslatokat.</div>
+        <div className="about-item image-box"><img src="../img/attilakep.jfif" alt="Attila" className='attilakep'/></div>
 
         {/* Zsombor */}
-        <div className="about-item image-box">Kép Zsomborról</div>
+        <div className="about-item image-box"> 
+          <img src="../img/zsomborkep.jfif" alt="Zsombor" className='zsomborkep'/>
+        </div>
         <div className="about-item text-box">Sziasztok Zsombor vagyok.</div>
 
         {/* Tamás */}
-        <div className="about-item text-box">Sziasztok Tamás vagyok.</div>
-        <div className="about-item image-box">Kép Tamásról</div>
+        <div className="about-item text-box">Haberle Tamás vagyok, szenvedélyem a programozás és örömmel vettem részt ebben a közös mestermunka projektben. A célom, hogy a projekt során megszerzett tapasztalatokat a következő munkáimban is hasznosítsam. A csapatmunkában új tapasztalatokat szereztem és remélem, hogy az elkészült weboldal elnyeri az emberek tetszést.
+        </div>
+        <div className="about-item image-box"><img src="../img/tamaskep.jfif" alt="Tamás" className='tamaskep'/></div>
       </div>
 
 
