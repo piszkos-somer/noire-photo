6540640d76db516965117343aee7ae30cdd70a0c
Kzsombor5 <kabai.zsombi@gmail.com>
2025-12-02 14:05:09 +0100

    Módosított adatbázis, backend módosításra vár

--- noire.sql
+++ noire.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Gép: 127.0.0.1:3307
--- Létrehozás ideje: 2025. Nov 13. 13:37
+-- Létrehozás ideje: 2025. Dec 02. 13:56
 -- Kiszolgáló verziója: 10.4.28-MariaDB
 -- PHP verzió: 8.2.4
 
@@ -50,33 +50,34 @@ INSERT INTO `comments` (`id`, `user_id`, `image_id`, `comment`, `upload_date`) V
 (9, 4, 12, 'Nagyon szép ügyes vagy!!', '2025-11-13 11:57:32'),
 (10, 4, 8, 'Uhhh. De komoly kép lett.', '2025-11-13 12:00:17'),
 (11, 3, 10, 'Nagyon aranyos😍', '2025-11-13 12:01:34'),
-(12, 3, 9, 'Kezdőként nagyon jó kép. Így tovább. Remélem láthatunk még tőled képeket.', '2025-11-13 12:02:44'),
-(13, 1, 13, 'Szerintem a rekeszérték túl kicsi, egy ilyen képhez illene ha nagyobb lenne, de maga a kép nagyon jó lett, 10/9.5!', '2025-11-13 12:48:50');
+(12, 3, 9, 'Kezdőként nagyon jó kép. Így tovább. Remélem láthatunk még tőled képeket.', '2025-11-13 12:02:44');
 
 -- --------------------------------------------------------
 
 --
--- Tábla szerkezet ehhez a táblához `comment_likes`
+-- Tábla szerkezet ehhez a táblához `comment_votes`
 --
 
-CREATE TABLE `comment_likes` (
+CREATE TABLE `comment_votes` (
   `id` int(10) UNSIGNED NOT NULL,
   `user_id` int(10) UNSIGNED NOT NULL,
-  `comment_id` int(10) UNSIGNED NOT NULL
+  `comment_id` int(10) UNSIGNED NOT NULL,
+  `vote` tinyint(4) NOT NULL DEFAULT 1,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- A tábla adatainak kiíratása `comment_likes`
+-- A tábla adatainak kiíratása `comment_votes`
 --
 
-INSERT INTO `comment_likes` (`id`, `user_id`, `comment_id`) VALUES
-(45, 1, 1),
-(46, 1, 2),
-(39, 2, 1),
-(54, 3, 5),
-(51, 4, 1),
-(52, 4, 3),
-(53, 4, 5);
+INSERT INTO `comment_votes` (`id`, `user_id`, `comment_id`, `vote`, `created_at`) VALUES
+(39, 2, 1, 1, '2025-12-02 12:54:44'),
+(45, 1, 1, 1, '2025-12-02 12:54:44'),
+(46, 1, 2, 1, '2025-12-02 12:54:44'),
+(51, 4, 1, 1, '2025-12-02 12:54:44'),
+(52, 4, 3, 1, '2025-12-02 12:54:44'),
+(53, 4, 5, 1, '2025-12-02 12:54:44'),
+(54, 3, 5, 1, '2025-12-02 12:54:44');
 
 -- --------------------------------------------------------
 
@@ -109,87 +110,27 @@ CREATE TABLE `images` (
   `title` varchar(255) NOT NULL,
   `description` text DEFAULT NULL,
   `upload_date` datetime NOT NULL DEFAULT current_timestamp(),
-  `url` varchar(255) NOT NULL,
-  `likes` int(10) UNSIGNED NOT NULL DEFAULT 0
+  `url` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- A tábla adatainak kiíratása `images`
 --
 
-INSERT INTO `images` (`id`, `user_id`, `title`, `description`, `upload_date`, `url`, `likes`) VALUES
-(2, 1, 'Mercedes', 'A legszebb merci amit valaha láttam!', '2025-10-27 17:43:22', '/images/1761579802620.jpg', 1),
-(3, 1, 'Tájkép', 'Ezt a képet egy másik országban készítettem, lenyűgöző volt a látvány!', '2025-10-27 19:02:33', '/images/1761584553646.jpg', 2),
-(4, 1, 'Bringás kép', 'Itt egy bringás kép rólam. Szerintetek jó szögből készült a kép? Vélemények?', '2025-10-27 19:03:49', '/images/1761584629316.jpg', 2),
-(5, 2, 'Tó az erdőben', 'Ezt a képet Kanadában készítettem, lenyűgöző látvánnyal.\r\n\r\nZáridő: kb. 10–30 másodperc\r\nRekesz: f/8 – f/1\r\nISO: 100\r\nGyújtótávolság: 18–24 mm ', '2025-10-27 19:13:21', '/images/1761585201828.jpg', 2),
-(6, 2, 'Izlandi hegység', 'Záridő: kb. 1/60 – 1/125 mp\r\nRekeszérték: f/8 – f/11\r\nISO érzékenység: 100 – 200\r\nGyújtótávolság: kb. 24–35 mm (nagylátószög)\r\nFehéregyensúly: napfény (kb. 5500 K)', '2025-10-27 19:41:21', '/images/1761586881317.jpg', 4),
-(7, 3, 'Nissan GTR R35', 'Szerintetek megpályázhatok egy versenyt ezzel a képpel?\r\nFényképezőgép: Sony A7R IV (Full Frame)\r\nObjektív: Sony FE 24–70mm f/2.8 GM\r\nGyújtótávolság: 35 mm\r\nRekesz (Aperture): f/5.6 — hogy az autó teljes élességben maradjon, de a háttér enyhén elmosódjon\r\nZáridő: 1/125 s — épp elég ahhoz, hogy kézből is éles legyen, miközben megőrzi a természetes fényeket\r\nISO: 200 — alacsony zaj, tiszta tónusok érdekében\r\nFehéregyensúly: Naplemente előbeállítás (kb. 6000K), hogy melegebb árnyalatokat kapjak\r\nUtómunka:\r\nEnyhe kontrasztnövelés és szaturáció az autó kiemelésére\r\nÁrnyékok világosítása, hogy a részletek ne vesszenek el\r\nÉgbolt enyhe színkiemelése a naplemente hangulatának fokozására', '2025-11-02 15:13:48', '/images/1762092828512.jpg', 2),
-(8, 3, 'Görögország', 'Telefonnal csináltam Görög nyaraláson során. Semmi extra beállítás csak egy jól elkapott pillanat.😎\nTelefonom: Samsung Galaxy S25', '2025-11-02 15:25:26', '/images/1762093526424.jpg', 2),
-(9, 4, 'Naplementés Balaton', 'Tavaly nyáron készítettem ezt a naplementés képet a Balatonról a telefonommal.\r\nTelefon: Samsung Galaxy S22', '2025-11-02 15:40:36', '/images/1762094436028.jpg', 2),
-(10, 4, 'Teknős', 'Egyik nyaraláson csináltam ezt a képet erről az aranyos teknősről. Remélem tetszik nektek. A Samsung Galaxy S22-es telefonnal készítettem ezt a képet.', '2025-11-10 11:09:03', '/images/1762769343370.jfif', 3),
-(11, 4, 'Vérhold', '2025 szeptember 7-én látható volt Magyarországon vérhold és ezt próbáltam lencse végre kapni, kisebb nagyobb sikerrel. Ezt a képet egy Iphone 14-el csináltam.', '2025-11-10 11:13:33', '/images/1762769613542.jfif', 2),
-(12, 1, 'Lánchíd', 'Ezt a képet tegnap este készítettem, hosszú expozícióval az Iphone 13 telefonommal :)', '2025-11-10 12:07:48', '/images/1762772868073.jpg', 2),
-(13, 3, 'Kékes', 'Smash or Pass?\nBeállítások:\nFényképezőgép: Canon EOS R6.\nObjektív: RF 24–105mm f/4 L IS USM.\nGyújtótávolság: 35 mm.\nRekesz: f/8 – részletgazdag, mély élességtartomány.\nZáridő: 1/100 s.\nISO: 200.\nFehéregyensúly: Árnyék / Naplemente mód (~6000K).\nUtómunka:\nMeleg színtónus kiemelése a naplemente hangulatához.', '2025-11-13 12:09:02', '/images/1763032142632.webp', 2),
-(14, 3, 'Vaddisznó', 'Nem rég egy barátommal vadászni jártam. Ő vadász én pedig egy fotós és ez lett a legjobb kép. Beállítások:\r\nFényképezőgép: Nikon D750.\r\nObjektív: Nikkor 70–200mm f/2.8.\r\nGyújtótávolság: 150 mm.\r\nRekesz: f/3.5 — sekély mélységélesség, hogy az állat kiemelkedjen a háttérből.\r\nZáridő: 1/500 s — az esetleges mozgás befagyasztásához. Fókusz: állat szemein.', '2025-11-13 12:14:38', '/images/1763032478390.webp', 2);
-
--- --------------------------------------------------------
-
---
--- Tábla szerkezet ehhez a táblához `image_likes`
---
-
-CREATE TABLE `image_likes` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `user_id` int(10) UNSIGNED NOT NULL,
-  `image_id` int(10) UNSIGNED NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- A tábla adatainak kiíratása `image_likes`
---
-
-INSERT INTO `image_likes` (`id`, `user_id`, `image_id`) VALUES
-(140, 1, 3),
-(129, 1, 4),
-(171, 1, 5),
-(182, 1, 6),
-(183, 1, 10),
-(186, 1, 11),
-(194, 1, 13),
-(195, 1, 14),
-(72, 2, 3),
-(103, 2, 4),
-(158, 2, 5),
-(157, 2, 6),
-(173, 3, 2),
-(172, 3, 6),
-(174, 3, 7),
-(175, 3, 8),
-(190, 3, 9),
-(193, 3, 10),
-(188, 3, 12),
-(191, 3, 13),
-(192, 3, 14),
-(177, 4, 6),
-(178, 4, 7),
-(179, 4, 8),
-(176, 4, 9),
-(181, 4, 10),
-(180, 4, 11),
-(187, 4, 12);
-
--- --------------------------------------------------------
-
---
--- Tábla szerkezet ehhez a táblához `image_reactions`
---
-
-CREATE TABLE `image_reactions` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `image_id` int(10) UNSIGNED NOT NULL,
-  `user_id` int(10) UNSIGNED NOT NULL,
-  `reaction_id` int(10) UNSIGNED NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+INSERT INTO `images` (`id`, `user_id`, `title`, `description`, `upload_date`, `url`) VALUES
+(2, 1, 'Mercedes', 'A legszebb merci amit valaha láttam!', '2025-10-27 17:43:22', '/images/1761579802620.jpg'),
+(3, 1, 'Tájkép', 'Ezt a képet egy másik országban készítettem, lenyűgöző volt a látvány!', '2025-10-27 19:02:33', '/images/1761584553646.jpg'),
+(4, 1, 'Bringás kép', 'Itt egy bringás kép rólam. Szerintetek jó szögből készült a kép? Vélemények?', '2025-10-27 19:03:49', '/images/1761584629316.jpg'),
+(5, 2, 'Tó az erdőben', 'Ezt a képet Kanadában készítettem, lenyűgöző látvánnyal.\r\n\r\nZáridő: kb. 10–30 másodperc\r\nRekesz: f/8 – f/1\r\nISO: 100\r\nGyújtótávolság: 18–24 mm ', '2025-10-27 19:13:21', '/images/1761585201828.jpg'),
+(6, 2, 'Izlandi hegység', 'Záridő: kb. 1/60 – 1/125 mp\r\nRekeszérték: f/8 – f/11\r\nISO érzékenység: 100 – 200\r\nGyújtótávolság: kb. 24–35 mm (nagylátószög)\r\nFehéregyensúly: napfény (kb. 5500 K)', '2025-10-27 19:41:21', '/images/1761586881317.jpg'),
+(7, 3, 'Nissan GTR R35', 'Szerintetek megpályázhatok egy versenyt ezzel a képpel?\r\nFényképezőgép: Sony A7R IV (Full Frame)\r\nObjektív: Sony FE 24–70mm f/2.8 GM\r\nGyújtótávolság: 35 mm\r\nRekesz (Aperture): f/5.6 — hogy az autó teljes élességben maradjon, de a háttér enyhén elmosódjon\r\nZáridő: 1/125 s — épp elég ahhoz, hogy kézből is éles legyen, miközben megőrzi a természetes fényeket\r\nISO: 200 — alacsony zaj, tiszta tónusok érdekében\r\nFehéregyensúly: Naplemente előbeállítás (kb. 6000K), hogy melegebb árnyalatokat kapjak\r\nUtómunka:\r\nEnyhe kontrasztnövelés és szaturáció az autó kiemelésére\r\nÁrnyékok világosítása, hogy a részletek ne vesszenek el\r\nÉgbolt enyhe színkiemelése a naplemente hangulatának fokozására', '2025-11-02 15:13:48', '/images/1762092828512.jpg'),
+(8, 3, 'Görögország', 'Telefonnal csináltam Görög nyaraláson során. Semmi extra beállítás csak egy jól elkapott pillanat.😎\nTelefonom: Samsung Galaxy S25', '2025-11-02 15:25:26', '/images/1762093526424.jpg'),
+(9, 4, 'Naplementés Balaton', 'Tavaly nyáron készítettem ezt a naplementés képet a Balatonról a telefonommal.\r\nTelefon: Samsung Galaxy S22', '2025-11-02 15:40:36', '/images/1762094436028.jpg'),
+(10, 4, 'Teknős', 'Egyik nyaraláson csináltam ezt a képet erről az aranyos teknősről. Remélem tetszik nektek. A Samsung Galaxy S22-es telefonnal készítettem ezt a képet.', '2025-11-10 11:09:03', '/images/1762769343370.jfif'),
+(11, 4, 'Vérhold', '2025 szeptember 7-én látható volt Magyarországon vérhold és ezt próbáltam lencse végre kapni, kisebb nagyobb sikerrel. Ezt a képet egy Iphone 14-el csináltam.', '2025-11-10 11:13:33', '/images/1762769613542.jfif'),
+(12, 1, 'Lánchíd', 'Ezt a képet tegnap este készítettem, hosszú expozícióval az Iphone 13 telefonommal :)', '2025-11-10 12:07:48', '/images/1762772868073.jpg'),
+(13, 3, 'Kékes', 'Smash or Pass?\nBeállítások:\nFényképezőgép: Canon EOS R6.\nObjektív: RF 24–105mm f/4 L IS USM.\nGyújtótávolság: 35 mm.\nRekesz: f/8 – részletgazdag, mély élességtartomány.\nZáridő: 1/100 s.\nISO: 200.\nFehéregyensúly: Árnyék / Naplemente mód (~6000K).\nUtómunka:\nMeleg színtónus kiemelése a naplemente hangulatához.', '2025-11-13 12:09:02', '/images/1763032142632.webp'),
+(14, 3, 'Vaddisznó', 'Nem rég egy barátommal vadászni jártam. Ő vadász én pedig egy fotós és ez lett a legjobb kép. Beállítások:\r\nFényképezőgép: Nikon D750.\r\nObjektív: Nikkor 70–200mm f/2.8.\r\nGyújtótávolság: 150 mm.\r\nRekesz: f/3.5 — sekély mélységélesség, hogy az állat kiemelkedjen a háttérből.\r\nZáridő: 1/500 s — az esetleges mozgás befagyasztásához. Fókusz: állat szemein.', '2025-11-13 12:14:38', '/images/1763032478390.webp');
 
 -- --------------------------------------------------------
 
@@ -250,13 +191,48 @@ INSERT INTO `image_tags` (`image_id`, `tag_id`) VALUES
 -- --------------------------------------------------------
 
 --
--- Tábla szerkezet ehhez a táblához `reactions`
+-- Tábla szerkezet ehhez a táblához `image_votes`
 --
 
-CREATE TABLE `reactions` (
+CREATE TABLE `image_votes` (
   `id` int(10) UNSIGNED NOT NULL,
-  `reaction_type` varchar(50) NOT NULL
+  `user_id` int(10) UNSIGNED NOT NULL,
+  `image_id` int(10) UNSIGNED NOT NULL,
+  `vote` tinyint(4) NOT NULL DEFAULT 1,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- A tábla adatainak kiíratása `image_votes`
+--
+
+INSERT INTO `image_votes` (`id`, `user_id`, `image_id`, `vote`, `created_at`) VALUES
+(72, 2, 3, 1, '2025-12-02 12:54:44'),
+(103, 2, 4, 1, '2025-12-02 12:54:44'),
+(129, 1, 4, 1, '2025-12-02 12:54:44'),
+(140, 1, 3, 1, '2025-12-02 12:54:44'),
+(157, 2, 6, 1, '2025-12-02 12:54:44'),
+(158, 2, 5, 1, '2025-12-02 12:54:44'),
+(171, 1, 5, 1, '2025-12-02 12:54:44'),
+(172, 3, 6, 1, '2025-12-02 12:54:44'),
+(173, 3, 2, 1, '2025-12-02 12:54:44'),
+(174, 3, 7, 1, '2025-12-02 12:54:44'),
+(175, 3, 8, 1, '2025-12-02 12:54:44'),
+(176, 4, 9, 1, '2025-12-02 12:54:44'),
+(177, 4, 6, 1, '2025-12-02 12:54:44'),
+(178, 4, 7, 1, '2025-12-02 12:54:44'),
+(179, 4, 8, 1, '2025-12-02 12:54:44'),
+(180, 4, 11, 1, '2025-12-02 12:54:44'),
+(181, 4, 10, 1, '2025-12-02 12:54:44'),
+(182, 1, 6, 1, '2025-12-02 12:54:44'),
+(183, 1, 10, 1, '2025-12-02 12:54:44'),
+(186, 1, 11, 1, '2025-12-02 12:54:44'),
+(187, 4, 12, 1, '2025-12-02 12:54:44'),
+(188, 3, 12, 1, '2025-12-02 12:54:44'),
+(190, 3, 9, 1, '2025-12-02 12:54:44'),
+(191, 3, 13, 1, '2025-12-02 12:54:44'),
+(192, 3, 14, 1, '2025-12-02 12:54:44'),
+(193, 3, 10, 1, '2025-12-02 12:54:44');
 
 -- --------------------------------------------------------
 
@@ -342,11 +318,12 @@ ALTER TABLE `comments`
   ADD KEY `image_id` (`image_id`);
 
 --
--- A tábla indexei `comment_likes`
+-- A tábla indexei `comment_votes`
 --
-ALTER TABLE `comment_likes`
+ALTER TABLE `comment_votes`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `unique_comment_like` (`user_id`,`comment_id`),
+  ADD UNIQUE KEY `unique_comment_vote` (`user_id`,`comment_id`),
   ADD KEY `comment_id` (`comment_id`);
 
 --
@@ -365,24 +342,6 @@ ALTER TABLE `images`
   ADD KEY `user_id` (`user_id`);
 
 --
--- A tábla indexei `image_likes`
---
-ALTER TABLE `image_likes`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `unique_like` (`user_id`,`image_id`),
-  ADD UNIQUE KEY `unique_user_image_like` (`user_id`,`image_id`),
-  ADD KEY `image_id` (`image_id`);
-
---
--- A tábla indexei `image_reactions`
---
-ALTER TABLE `image_reactions`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `unique_user_image` (`user_id`,`image_id`),
-  ADD KEY `image_id` (`image_id`),
-  ADD KEY `reaction_id` (`reaction_id`);
-
---
 -- A tábla indexei `image_tags`
 --
 ALTER TABLE `image_tags`
@@ -390,11 +349,14 @@ ALTER TABLE `image_tags`
   ADD KEY `tag_id` (`tag_id`);
 
 --
--- A tábla indexei `reactions`
+-- A tábla indexei `image_votes`
 --
-ALTER TABLE `reactions`
+ALTER TABLE `image_votes`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `reaction_type` (`reaction_type`);
+  ADD UNIQUE KEY `unique_like` (`user_id`,`image_id`),
+  ADD UNIQUE KEY `unique_user_image_like` (`user_id`,`image_id`),
+  ADD UNIQUE KEY `unique_image_vote` (`user_id`,`image_id`),
+  ADD KEY `image_id` (`image_id`);
 
 --
 -- A tábla indexei `tags`
@@ -422,9 +384,9 @@ ALTER TABLE `comments`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
--- AUTO_INCREMENT a táblához `comment_likes`
+-- AUTO_INCREMENT a táblához `comment_votes`
 --
-ALTER TABLE `comment_likes`
+ALTER TABLE `comment_votes`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=55;
 
 --
@@ -440,22 +402,10 @@ ALTER TABLE `images`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
--- AUTO_INCREMENT a táblához `image_likes`
+-- AUTO_INCREMENT a táblához `image_votes`
 --
-ALTER TABLE `image_likes`
+ALTER TABLE `image_votes`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=196;
-
---
--- AUTO_INCREMENT a táblához `image_reactions`
---
-ALTER TABLE `image_reactions`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT a táblához `reactions`
---
-ALTER TABLE `reactions`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT a táblához `tags`
@@ -481,11 +431,10 @@ ALTER TABLE `comments`
   ADD CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`image_id`) REFERENCES `images` (`id`) ON DELETE CASCADE;
 
 --
--- Megkötések a táblához `comment_likes`
+-- Megkötések a táblához `comment_votes`
 --
-ALTER TABLE `comment_likes`
-  ADD CONSTRAINT `comment_likes_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `comment_likes_ibfk_2` FOREIGN KEY (`comment_id`) REFERENCES `comments` (`id`) ON DELETE CASCADE;
+ALTER TABLE `comment_votes`
+  ADD CONSTRAINT `comment_votes_ibfk_2` FOREIGN KEY (`comment_id`) REFERENCES `comments` (`id`) ON DELETE CASCADE;
 
 --
 -- Megkötések a táblához `follows`
@@ -501,26 +450,17 @@ ALTER TABLE `images`
   ADD CONSTRAINT `images_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
 
 --
--- Megkötések a táblához `image_likes`
---
-ALTER TABLE `image_likes`
-  ADD CONSTRAINT `image_likes_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `image_likes_ibfk_2` FOREIGN KEY (`image_id`) REFERENCES `images` (`id`) ON DELETE CASCADE;
-
---
--- Megkötések a táblához `image_reactions`
---
-ALTER TABLE `image_reactions`
-  ADD CONSTRAINT `image_reactions_ibfk_1` FOREIGN KEY (`image_id`) REFERENCES `images` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `image_reactions_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `image_reactions_ibfk_3` FOREIGN KEY (`reaction_id`) REFERENCES `reactions` (`id`) ON DELETE CASCADE;
-
---
 -- Megkötések a táblához `image_tags`
 --
 ALTER TABLE `image_tags`
   ADD CONSTRAINT `image_tags_ibfk_1` FOREIGN KEY (`image_id`) REFERENCES `images` (`id`) ON DELETE CASCADE,
   ADD CONSTRAINT `image_tags_ibfk_2` FOREIGN KEY (`tag_id`) REFERENCES `tags` (`id`) ON DELETE CASCADE;
+
+--
+-- Megkötések a táblához `image_votes`
+--
+ALTER TABLE `image_votes`
+  ADD CONSTRAINT `image_votes_ibfk_2` FOREIGN KEY (`image_id`) REFERENCES `images` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
