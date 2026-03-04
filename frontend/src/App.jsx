025ea01821fe9c7f7bec09a5c986e1f9eac5608b
piszkos-somer <peratt816@hengersor.hu>
2026-03-04 10:16:45 +0100

    footer login fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import AnimatedLinesBackground from "./components/AnimatedLinesBackground";
-import {React, useEffect} from "react";
+import { React, useEffect } from "react";
 import { Routes, Route, useNavigate } from "react-router-dom";
 import { UserProvider } from "./context/UserContext";
 import NavbarNoire from "./components/Navbar";
@@ -19,33 +19,34 @@ function App() {
 
   useEffect(() => {
     logoutIfExpired(navigate);
-
     const interval = setInterval(() => logoutIfExpired(navigate), 60000);
     return () => clearInterval(interval);
   }, [navigate]);
 
   return (
-    <>
-    <AnimatedLinesBackground lineCount={38} speed={0.6} amplitude={0.22} alpha={0.6} color="#2a6cff" />
-    <UserProvider>
-      <NavbarNoire />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/About" element={<About />} />
-        <Route path="/Registration" element={<Registration />} />
-        <Route path="/Upload" element={<Upload />} />
-        <Route path="/Login" element={<Login />} />
-        <Route path="/Profile" element={<Profile />} />
-        <Route path="/profile/:id" element={<ViewProfile />} />
-        <Route path="/viewprofile/:id" element={<ViewProfile />} />
-        <Route path="/browse" element={<Browse />} />
-        <Route path="/browse/:tag" element={<Browse />} />
-        
-
-      </Routes>
-      <Footer />
-    </UserProvider>
-    </>
+    <div className="min-vh-100 d-flex flex-column">
+      <AnimatedLinesBackground lineCount={38} speed={0.6} amplitude={0.22} alpha={0.6} color="#2a6cff" />
+      <UserProvider>
+        <NavbarNoire />
+        <div className="flex-grow-1 d-flex flex-column">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/About" element={<About />} />
+            <Route path="/Registration" element={<Registration />} />
+            <Route path="/Upload" element={<Upload />} />
+            <Route path="/Login" element={<Login />} />
+            <Route path="/Profile" element={<Profile />} />
+            <Route path="/profile/:id" element={<ViewProfile />} />
+            <Route path="/viewprofile/:id" element={<ViewProfile />} />
+            <Route path="/browse" element={<Browse />} />
+            <Route path="/browse/:tag" element={<Browse />} />
+          </Routes>
+        </div>
+        <div className="mt-auto">
+          <Footer />
+        </div>
+      </UserProvider>
+    </div>
   );
 }
 
